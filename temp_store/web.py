2f05465724a1be4062ebf26968d2a91f28553f21
Patrick Stünkel <webminz@googlemail.com>
2024-02-26 17:46:23 +0100

    new web api

--- temp_store/web.py
+++ temp_store/web.py
@@ -1,36 +1,51 @@
-from typing import  Optional
+import os
+from datetime import date, datetime
 from fastapi import FastAPI 
-from pydantic import BaseModel
 
 from fastapi.staticfiles import StaticFiles
+from temp_store.client import MetClient
 
+from temp_store.domain import Location, TemperatureRecording
+from temp_store.store import TemperatureStore
 
+from dotenv import  load_dotenv
 
-class User(BaseModel):
-    email : str 
-    id : int
-
-
+load_dotenv()
 app = FastAPI()
+store = TemperatureStore()
+retriever = MetClient(os.environ['MET_CLIENT_ID'])
 
+# Serve static files
 app.mount("/static", StaticFiles(directory="pages"))
 
 @app.get("/")
 def hello_word():
-    return "Hei!"
+    return "Hei verden!"
 
-@app.get("/user") 
-def get_user() -> User:
-    return User(id=23, email="ole@nordmann.no")
+@app.get("/greet/{name}")
+def greet(name: str = "verden"):
+    return f"Hei {name}!"
 
-@app.get("/weather")
-def get_weather_bergen(location: Optional[str] = None) -> str:
-    if location == "bergen":
-        return "rainy"
-    elif location == "oslo":
-        return "snowy"
-    else:
-        return "dunno"
+
+@app.get("/health") 
+def get_user() -> bool:
+    return True
+
+@app.get("/{latitude}/{longitude}")
+def get_temperature(latitude: float, longitude: float, timestamp: datetime) -> TemperatureRecording | None:
+    return store.retrieve(Location(longitude, latitude), timestamp)
+
+@app.post("/{latitude}/{longitude}")
+def collect_temperature(latitude: float, longitude: float, date: date):
+    location = Location(longitude, latitude)
+    print(f"Updating data for location ({latitude}, {longitude}) from Frost API")
+    for h in range(23):
+        ts = datetime(date.year, date.month, date.day, h)
+        ts_untl = datetime(date.year, date.month, date.day, h, 10)
+        data = retriever.retrieve(location, ts, ts_untl)
+        if len(data) > 0:
+            print(f"Retrieved data for ({latitude}, {longitude}) {ts.isoformat()}")
+            store.store(location, ts, data[0])
 
 
 
